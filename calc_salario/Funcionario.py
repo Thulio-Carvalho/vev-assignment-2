cb8d164f65475a4bdcd81081131dffd9ddeefef6
Thúlio Carvalho <thulioicc@gmail.com>
2022-10-24 19:08:01 -0300

    refactor dev sallary calculator code

--- calc_salario/Funcionario.py
+++ calc_salario/Funcionario.py
@@ -5,7 +5,21 @@ class Funcionario:
         self.salario_base = salario_base
         self.cargo = cargo.lower() 
 
+        self.DISCOUNT_RATE = {
+            'desenvolvedor': {
+                'base_val_threshold': 3000,
+                'lt_discount': 0.1,
+                'gte_discount': 0.2
+            }
+        }
+
     def salario_liq(self):
-        if self.cargo == 'desenvolvedor':
-            if self.salario_base >= 3000: return self.salario_base * (1.0 - 0.2)
-            return self.salario_base * (1.0 - 0.1)
+        is_base_lower_than_threshold = self.salario_base < self.DISCOUNT_RATE[self.cargo]['base_val_threshold']
+        def __lt_sallary(salario_base): 
+            return salario_base * (1.0 - self.DISCOUNT_RATE[self.cargo]['lt_discount'])
+        def __gte_sallary(salario_base): 
+            return salario_base * (1.0 - self.DISCOUNT_RATE[self.cargo]['gte_discount'])
+
+        if is_base_lower_than_threshold:
+            return __lt_sallary(self.salario_base)
+        return __gte_sallary(self.salario_base)
