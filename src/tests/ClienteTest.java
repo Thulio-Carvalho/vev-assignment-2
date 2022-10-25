d8c5e15424504a9cdb1ebedc094851c08e4118e4
Augusto Santos <augusto.santos@ccc.ufcg.edu.br>
2022-10-24 21:38:08 -0300

    Refactoring nomes

--- src/tests/ClienteTest.java
+++ src/tests/ClienteTest.java
@@ -25,75 +25,75 @@ class ClienteTest {
 	
 	@Test
 	void testClienteAddFatura() {
-		Cliente c = new Cliente("Augusto", "2022-08-24", Estados.PB);
+		Cliente cliente = new Cliente("Augusto", "2022-08-24", Estados.PB);
 		Fatura fatura = new Fatura(1, 350, "2022-08-24");
-		c.adicionaFatura(fatura);
-		assertEquals(c.getFaturas().size(), 1);
+		cliente.adicionaFatura(fatura);
+		assertEquals(cliente.getFaturas().size(), 1);
 	}
 	
 	@Test
 	void testFiltraFaturasMenoresQue2000() {
-		Cliente c = new Cliente("Augusto", "2022-08-24", Estados.PB);
+		Cliente cliente = new Cliente("Augusto", "2022-08-24", Estados.PB);
 		
 		Fatura fatura1 = new Fatura(1, 1999.99, "2022-08-24");
 		Fatura fatura2 = new Fatura(2, 2000, "2022-08-24");
 		Fatura fatura3 = new Fatura(3, 2100, "2022-08-24");
 		
-		c.adicionaFatura(fatura1);
-		c.adicionaFatura(fatura2);
-		c.adicionaFatura(fatura3);
+		cliente.adicionaFatura(fatura1);
+		cliente.adicionaFatura(fatura2);
+		cliente.adicionaFatura(fatura3);
 		
-		c.filtraFaturas();
-		assertEquals(c.getFaturas().size(), 2);
+		cliente.filtraFaturas();
+		assertEquals(cliente.getFaturas().size(), 2);
 	}
 
 	@Test
 	void testFiltraFaturasEntre2000E2500() {
-		Cliente c = new Cliente("Augusto", "2022-10-24", Estados.PB);
+		Cliente cliente = new Cliente("Augusto", "2022-10-24", Estados.PB);
 		
 		Fatura fatura1 = new Fatura(1, 2000, "2022-08-10");
 		Fatura fatura2 = new Fatura(2, 2500, "2022-10-20");
 		
-		c.adicionaFatura(fatura1);
-		c.adicionaFatura(fatura2);
+		cliente.adicionaFatura(fatura1);
+		cliente.adicionaFatura(fatura2);
 		
-		c.filtraFaturas();
-		assertEquals(c.getFaturas().size(), 1);
+		cliente.filtraFaturas();
+		assertEquals(cliente.getFaturas().size(), 1);
 	}
 
 
 	@Test
 	void testFiltraFaturasEntre2500E3000() {
-		Cliente c1 = new Cliente("Augusto", "2022-07-25", Estados.PB);
-		Cliente c2 = new Cliente("Gomes", "2022-10-24", Estados.PB);
+		Cliente cliente1 = new Cliente("Augusto", "2022-07-25", Estados.PB);
+		Cliente cliente2 = new Cliente("Gomes", "2022-10-24", Estados.PB);
 		
 		Fatura fatura1 = new Fatura(1, 2500, "2022-10-24");
 		Fatura fatura2 = new Fatura(2, 3000, "2022-10-20");
 		
-		c1.adicionaFatura(fatura1);
-		c1.adicionaFatura(fatura2);
+		cliente1.adicionaFatura(fatura1);
+		cliente1.adicionaFatura(fatura2);
 		
-		c2.adicionaFatura(fatura1);
-		c2.adicionaFatura(fatura2);
+		cliente2.adicionaFatura(fatura1);
+		cliente2.adicionaFatura(fatura2);
 		
-		c1.filtraFaturas();
-		c2.filtraFaturas();
+		cliente1.filtraFaturas();
+		cliente2.filtraFaturas();
 		
-		assertEquals(c1.getFaturas().size(), 2);
-		assertEquals(c2.getFaturas().size(), 0);
+		assertEquals(cliente1.getFaturas().size(), 2);
+		assertEquals(cliente2.getFaturas().size(), 0);
 	}
 	
 	@Test
-	void testFiltraFaturasMaiorQue4000ClienteSul() {
-		Cliente c1 = new Cliente("Santos", "2022-10-24", Estados.RS);
+	void testFiltraFaturasMaioresQue4000ClienteSul() {
+		Cliente cliente = new Cliente("Santos", "2022-10-24", Estados.RS);
 		
 		Fatura fatura1 = new Fatura(1, 4000, "2022-08-24");
 		Fatura fatura2 = new Fatura(2, 4200, "2022-10-20");
 		
-		c1.adicionaFatura(fatura1);
-		c1.adicionaFatura(fatura2);
+		cliente.adicionaFatura(fatura1);
+		cliente.adicionaFatura(fatura2);
 		
-		c1.filtraFaturas();
+		cliente.filtraFaturas();
 		assertEquals(this.cliente.getFaturas().size(), 0);
 	}
 	
